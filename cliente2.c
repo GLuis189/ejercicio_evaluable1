6212f50fd952d1e756b4f95c37953fb185645806
GLuis189 <189luis189@gmail.com>
2024-03-16 16:41:18 +0100

    as

--- cliente2.c
+++ cliente2.c
@@ -7,13 +7,33 @@
 
 
 int main(){
-    // provar el set_value asi int set_value(int key, char *value1, int N_value2, double *V_value_2).
-    printf("c2");
-    if(set_value(3, "hola c2", 33, (double[]){1.0, 2.0, 3.0}) == 0){
-        printf("Seteado valor\n");
+    if(set_value(3, "hola c2", 3, (double[]){1.0, 2.0, 3.0}) == 0){
+        printf("Seteado valor 3\n");
+    }
+    else{
+        printf("Error al setear valor 3\n");
     }
     if(set_value(4, "hola2 c2", 4, (double[]){1.0, 2.0, 3.0, 4.0}) == 0){
-        printf("Seteado valor 2\n");
+        printf("Seteado valor 4\n");
+    }
+    else{
+        printf("Error al setear valor 4\n");
+    }
+    if(exist(2) == 0){
+        printf("La clave 2 existe.\n");
+    }
+    if(exist(3) == 0){
+        printf("La clave 3 existe.\n");
+    }
+    if(delete_key(3) == 0){
+        printf("Clave 3 eliminada con éxito.\n");
+    }
+    if(exist(3) == 0){
+        printf("La clave 3 existe.\n");
+    }
+    else
+    {
+        printf("La clave 3 no existe.\n");
     }
     return 0;
 }
